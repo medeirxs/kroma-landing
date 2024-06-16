dca8d3364a85581f62aaed22bf173b92eaa15819
Matheus Medeiros <mtmedeirosgs@gmail.com>
2024-06-16 13:17:27 -0300

    fix: Navbar opacity bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,49 @@
 import { ThemeProvider } from "@/components/providers/theme-provider"
 import { NavBar } from "@/components/navbar"
+import { Separator } from "./components/ui/separator"
+import { Button } from "./components/ui/button"
 
 export function App() {
   return (
-    <div className="flex h-screen justify-center">
+    <>
       <NavBar/>
-        <div className="flex flex-col">
-        <img src="https://i.pinimg.com/564x/43/bf/c1/43bfc1a6bfe8ce2a933cc3bdcdee45d4.jpg" className="w-[1920px]"/>
-        <img src="https://i.pinimg.com/564x/50/01/f2/5001f24ee5de4da07e955e66c33e668d.jpg" />
-        <img src="https://i.pinimg.com/564x/d6/23/19/d623199b4965941d8c9f63a4eda20de8.jpg" />
+
+      <div className="flex h-screen justify-center">
+        <div>
+          <div className="flex flex-col justify-center items-center w-screen h-full drop-shadow-lg z-0">
+
+            <h1 className="text-5xl font-bold">Tudo que voce precisa</h1>
+            <h1 className="text-5xl font-bold"> em um lugar só.</h1>
+
+            <p className="text-sm text-muted-foreground">Feito por designers, para designers.</p>
+
+            <div className="flex flex-col items-center gap-2 mt-5 text-sm">
+              <div className="flex gap-2">
+                <span><span className="font-extrabold">+200GB</span> Arquivos</span>
+                <Separator orientation="vertical"/>
+                <span>Softwares Atualizados</span>
+              </div>
+              <Separator orientation="horizontal"/>
+              <span>Aulas e Conteúdos <span className="font-extrabold">Exclusivos</span></span>
+            </div>
+
+          </div>
+          <div>
+            <h1>adssd</h1>
+            <h1>bb</h1>
+          </div>
+          <img src="https://i.pinimg.com/564x/43/bf/c1/43bfc1a6bfe8ce2a933cc3bdcdee45d4.jpg" className="w-[1920px]"/>
+        </div>
       </div>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme" children={undefined}/>
-    </div>
+
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme" children={undefined}/>
+    </>
   )
 }
+
+
+{/*<div className="flex flex-col">
+  <img src="https://i.pinimg.com/564x/43/bf/c1/43bfc1a6bfe8ce2a933cc3bdcdee45d4.jpg" className="w-[1920px]"/>
+  <img src="https://i.pinimg.com/564x/50/01/f2/5001f24ee5de4da07e955e66c33e668d.jpg" />
+  <img src="https://i.pinimg.com/564x/d6/23/19/d623199b4965941d8c9f63a4eda20de8.jpg" />
+</div>*/}
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,9 +2,10 @@ import { Button } from "./ui/button";
 
 export function NavBar(){
   return(
-    <div className="fixed flex justify-center p-16 w-screen bg-gradient-to-b from-black/70">
-      <div className="flex justify-between items-center -mt-12 w-[1024px] h-12  border-[0.5px] border-white/20 p-3 rounded-2xl backdrop-blur-md">
+    <div className="fixed z-10 flex item-center h-32 justify-center w-screen bg-gradient-to-b from-black/70">
+      <div className="mt-3 w-[1024px] h-12 flex justify-between items-center p-3 backdrop-blur-md rounded-2xl border border-white/20">
 
+        {/*Kroma Logo and NavBar Title*/}
         <div className="flex items-center gap-2">
           <div className="flex items-center justify-center h-6 w-6 rounded-md border border-white/50">
             <span style={{fontFamily: 'courier prime'}} className=" tracking-tighter text-xs mt-1">Kr</span>
@@ -12,6 +13,7 @@ export function NavBar(){
           <span className=" font-bold tracking-tighter text-sm">Kroma</span>
         </div>
 
+        {/*NavBar options*/}
         <div className="flex items-center gap-2">
           <Button className="text-sm font-semibold text-white/70 hover:bg-zinc-100/10 " variant="ghost">empty</Button>
           <Button className="text-sm font-semibold text-white/70 hover:bg-zinc-100/10 " variant="ghost">empty</Button>
@@ -19,7 +21,9 @@ export function NavBar(){
           <Button className="text-sm font-semibold text-white/70 hover:bg-zinc-100/10 " variant="ghost">empty</Button>
         </div>
 
+        {/*path to Kroma platform*/}
         <Button className="text-xs w-24 h-8">Plataforma</Button>
+
       </div>
     </div>
   )
