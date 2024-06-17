f6245a9a3ed32d7ec3bf1d2bbbbfed3cfc314c17
Matheus Medeiros <mtmedeirosgs@gmail.com>
2024-06-17 16:17:16 +0100

    feat:remove sucessfull page

--- src/pages/payment-sucessfull.tsx
+++
@@ -1,9 +0,0 @@
-export function AfterPay(){
-  return(
-    <>
-      <div className="w-screen h-screen flex justify-center items-center">
-        <div className="text-6xl">pagamento com sucesso</div>
-      </div>
-    </>
-  )
-}
